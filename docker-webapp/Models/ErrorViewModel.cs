45dbfdb8c99f1748d931934b3ce964877fddce78
yesh01b <yeswanthb40@gmail.com>
2025-01-07 19:02:06 +0530

    Add project files.

---
+++ docker-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace docker_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
